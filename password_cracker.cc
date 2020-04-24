c42ac1e29be8d6818c47dba2df910525ee4cce4d
Abigail McHugh <amchugh2@binghamton.edu>
2020-04-24 15:50:41 -0400

    commiting

--- password_cracker.cc
+++ password_cracker.cc
@@ -6,15 +6,19 @@
 #include <iostream>
 #include <ostream>
 
+#define PASSWORD_LEN 6
+
+// Generate random data to feed to simple brute force and dictionary brute force password crackers
 std::vector<std::string> generate_data(int len){
-	//static const char alpha[26] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
 	std::string str = "abcdefghijklmnopqrstuvwxyz";
 	std::vector<std::string> data;
 	for(int i = 0; i <= len; ++i){
 		std::string entry;
 		int pos;
-		data.push_back(entry);
-		}
+		while(entry.size() != PASSWORD_LEN){
+			pos = ((rand() % (str.size() - 1)));
+			entry += str.substr(pos,1);
+			}
 		data.push_back(entry);
 		}
 	return data;
@@ -24,7 +28,7 @@ std::vector<std::string> generate_data(int len){
 int main(int argc, char *argv[]){
 	std::vector<std::string> data = generate_data(10);
 	for(std::vector<std::string>::iterator it = data.begin(); it != data.end(); it++){
-		std::cout << *it << std::endl;
+		// call simple brute force
 	}
 }
 
