5b3f85ba754c1be42c9655c7085b111f1f9aa05e
Abigail McHugh <amchugh2@binghamton.edu>
2020-04-29 23:14:53 -0400

    almost freaking working

--- password_cracker.cc
+++ password_cracker.cc
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <ostream>
 #include <string_view>
+#include<bits/stdc++.h>
 
 #define PASSWORD_LIMIT 6
 
@@ -29,29 +30,50 @@ std::vector<std::string> generate_data(int data_size){
 	 
 }
 
+
+std::string permute(char[] alphabet, std::string prefix, int n, int k){
+	if(k == 0){
+		return prefix;
+	}
+	for(int i = 0; i < n; i++){
+		std::string newPrefix;
+		newPrefix = prefix + alphabet[i];
+		permute(alphabet, newPrefix, n, k - 1);
+	}
+}
+
+void print(char[] alphabet, int k, int n){
+	permute(alphabet, "", n, k);
+}
+/*
 // Simple brute-force password cracker
 // Given: A list of 1000 password to crack
 // String: If password is cracked
 void simple_brute_force(std::string password){
 	std::string alphabet = "abcdefghijklmnopqrstuvwxyz";
 	//char alphabet[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-	int len = 26;
-	//std::cout << "actual password is: " << password << std::endl;
+	int len = 7;
+	std::string prefix = "";
+	if(k == 0){
+		return;
+	}
 	for(int i = 0; i < len; i++){
-		for(int j = 1; j <= len - i; j++){
-			std::string_view guess = alphabet.substr(i,j);
-			//std::cout << "guess: " << guess << std::endl;
+		std::string new;
+		new = prefix + alphabet[i];
+
+
+	int n = 26;
+	for(int i = 0; i < n; i++){
+		for(int len = 1; len <= n - i ; len++){
+			std::string_view guess = alphabet.substr(i,len);
 			if(password == guess){
-				std::cout << "PASSWORD CRACKED " << "password is: " << password << std::endl;
+				std::cout << password << std::endl;
 			}
 		}
 	}
-
 }
 			
 
-
-/*
 //simple brute force
 //lowercase letters
 const char lowerCase[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
@@ -65,20 +87,19 @@ void options(unsigned int length, std::vector<std::string> password){
     options(length-1, appended);
   }
 }
-void crackPassword(){
-  while(1){
-    static unsigned int stringlength = 1;
-    options(stringlength, {""});
-    stringlength++;
-  }
-}
 */
 
 int main(int argc, char *argv[]){
         std::vector<std::string> data = generate_data(5000);
+	char alphabet[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+	int k = 1;
+	print(alphabet, k, 5); 
+
+	/*
 	for(int i = 0; i < data.size(); i++){
 		simple_brute_force(data.at(i));
 	}
+	*/
 	return 0;
 }
 
