e560970b969e8997302fd8c062f30346f683d7c7
Amelia Chesebro <acheseb1@cs.binghamton.edu>
2020-05-03 21:16:57 -0400

    both methods working :D

--- password_cracker.cc
+++ password_cracker.cc
@@ -30,7 +30,7 @@ void  generateData(int data_size, std::vector<std::string> dictionary){
 	for(int i = 0; i < data_size; ++i){
 		std::string entry;
 		int pos;
-		int size = 3;
+		int size = 4;
 		while(entry.size() != size){
 			pos = ((rand() % (str.size() - 1)));
 			entry += str.substr(pos,1);
@@ -39,7 +39,7 @@ void  generateData(int data_size, std::vector<std::string> dictionary){
 		
 		// randomly add dictionary keyword
 		int rand = std::rand() % 5 + 1;
-		if(i % 5 == 0 && i<dictionary.size()){
+		if(i % 5 == 0){
 			outputFile << dictionary.at(i) << std::endl;
 		}
 	}
@@ -48,7 +48,7 @@ void  generateData(int data_size, std::vector<std::string> dictionary){
 
 //generate permutations of length 6 from alphabet and feed to brute force checker 
 void bruteForce(char *perm, int startPos, std::string alphabet, std::string password){
-	if(startPos == 3) {
+	if(startPos == 4) {
 		std::string permString(perm);
 		if(password.compare(permString)== 0)
 			return;
@@ -65,7 +65,7 @@ void bruteForce(char *perm, int startPos, std::string alphabet, std::string pass
 }
 		
 
-bool dictionary(std::vector<std::string>& dict, std::string& password){
+bool dictionaryMethod(std::vector<std::string>& dict, std::string& password){
 	for(int i = 0; i < dict.size(); i++){
 		if(dict.at(i).compare(password)==0){
 			return true;
@@ -80,19 +80,19 @@ int main(int argc, char *argv[]){
 	std::vector<std::string> dict;
 	std::string entry;
 	while(getline(dictionary, entry)){
-		if(entry.length() == 3 && isAlpha(entry)){
+		if(entry.length() == 4 && isAlpha(entry)){
 			dict.push_back(entry);
-			std::cout<< "here"  << std::endl;
+			//std::cout << entry << std::endl;
 		}
 	} 
 	
 
-	int numPasswords = 500;
+	int numPasswords = 50;
 	std::string alphabet = "abcdefghijklmnopqrstuvwxyz";
 
        	generateData(numPasswords, dict);
 	
-	char permutation[4];
+	char permutation[5];
 	std::ifstream passwords("passwords.txt");
 	std::string password;
 	//int bfPasswordsCracked = 0;
@@ -116,19 +116,20 @@ int main(int argc, char *argv[]){
 	auto dictStart = std::chrono::high_resolution_clock::now();
 	std::cout << "----------Cracking Paswords Via Dictionary ----------" << std::endl;
 
-	while(getline(passwords, password)){
-		///dictionary(dict, password);
+	while(getline(file2, password)){
+		if(dictionaryMethod(dict, password))
+			dictPasswordsCracked++;
 	}
 
 	auto dictStop = std::chrono::high_resolution_clock::now();
 	std::chrono::duration<double> dictDiff = (dictStop - dictStart);
 	auto dictTotalTime = dictDiff.count();
-	auto dictAvgTime = bfTotalTime / dictPasswordsCracked;
+	auto dictAvgTime = dictTotalTime / numPasswords;
 
 	
 	std::cout << "---------- Brute Force ----------" << std::endl;
 	std::cout << "Passwords Cracked: " << numPasswords << std::endl;
-	std::cout << "Total Time to Crack Passwords: " << bfTotalTime << std::endl; 
+	std::cout << "Total Time to Crack Passwords (s): " << bfTotalTime << std::endl; 
 	std::cout << "Time per Password Cracked (ms):  " << bfAvgTime << std::endl;
 
 	std::cout << "---------- Dictionary ---------" << std::endl;
